5592f64b54136f79a10d8ef28fe4aae72cade4ee
Esen888 <esensharsheev808@gmail.com>
2023-05-05 15:30:30 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyC2d-qIUMtv1HHDBp74p6HbYbeL7XTNmjw",
+    appId: "1:259839654419:android:0a61dffb0c8589c9289de6",
+    messagingSenderId: "259839654419",
+    projectId: "bodypower-1bdb2",
+    storageBucket: "bodypower-1bdb2.appspot.com",
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/user_data_model.dart
@@ -0,0 +1,15 @@
+class UserDataModel {
+  final String displayName;
+  final String email;
+  final String emailVerified;
+  final String photoUrl;
+  final String phoneNumber;
+
+  UserDataModel(
+    this.displayName,
+    this.email,
+    this.emailVerified,
+    this.photoUrl,
+    this.phoneNumber,
+  );
+}
